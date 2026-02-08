2bb855fb49673d39b24e4e61ceed8cad8cbe2b5e
arrrre <arvid.johansson@telia.com>
2026-02-08 09:45:03 +0100

    Work on matlab copy.

--- matrix/matlab2.c
+++ matrix/matlab2.c
@@ -4,6 +4,11 @@
 
 #include "mat.h"
 
+bool starts_with(const char *str, const char *pre) {
+    size_t lenpre = strlen(pre), lenstr = strlen(str);
+    return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
+}
+
 int main() {
 	char input[256];
 	while (1) {
@@ -12,7 +17,12 @@ int main() {
         input[strcspn(input, "\n")] = 0;
         if (strlen(input) == 0) continue;
         if (strcmp(input, "q") == 0) break;
-		
+
+		if (starts_with(input, "clear")) {
+			printf("input: %s\n", input);
+		} else if (starts_with(input, "close")) {
+			printf("input: %s\n", input);
+		}
 	}
 
 	return 0;
